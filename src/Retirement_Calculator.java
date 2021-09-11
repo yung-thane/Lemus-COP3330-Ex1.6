67ebd51b1e2c0253ea518a34e35458f8eb6aeb1e
Ronald <ronald.cristobal.lemus@outlook.com>
2021-09-11 15:09:17 -0400

    Initial commit

---
+++ src/Retirement_Calculator.java
@@ -0,0 +1,25 @@
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class Retirement_Calculator {
+    public static void main(String[] args){
+        System.out.print("What is your current age? ");
+        Scanner ageInp = new Scanner(System.in);
+        String ageString = ageInp.nextLine();
+        int ageCurrent = Integer.parseInt(ageString);
+
+        System.out.print("At what age would you like to retire? ");
+        Scanner ageRetInp = new Scanner(System.in);
+        String ageRetString = ageRetInp.nextLine();
+        int ageRetire = Integer.parseInt(ageRetString);
+
+        int yearsTillRetire = ageRetire - ageCurrent;
+        System.out.printf("You have %d years left until you can retire.", yearsTillRetire);
+
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        int retireYear = currentYear + yearsTillRetire;
+        System.out.printf("It's %d, so you can retire in %d.", currentYear, retireYear);
+
+
+    }
+}
